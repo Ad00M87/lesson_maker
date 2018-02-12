04b00f766f6d10a2cfcf548820fa89aeb157f2c5
Adam Kimball <adamchasekimball@gmail.com>
2018-02-12 09:28:38 -0700

    instructions on Create Lesson

--- client/src/components/CreateLesson.js
+++ client/src/components/CreateLesson.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Header, Segment, Divider } from 'semantic-ui-react';
 
 class CreateLesson extends React.Component {
   render() {
     return(
-      <div>CreateLesson</div>
+      <Segment>
+        <Header as='h1'>Lesson Creator</Header>
+        <Divider />
+        <Header as='h3'>Add a title</Header>
+      </Segment>
     )
   }
 }
