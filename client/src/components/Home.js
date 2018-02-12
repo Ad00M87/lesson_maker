7f583b87af1e811a0c15d660658245d44c36fdaa
Adam Kimball <adamchasekimball@gmail.com>
2018-02-11 21:33:03 -0700

    planning home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Segment } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
     return (
       <Header as='h1' textAlign='center'>Home Component</Header>
+      <Segment>
+        <Header as='h3' textAlign='center'>Information about the site</Header>
+        <Header as='h4' textAlign='center'>Snippet Images</Header>
+      </Segment>
     );
   }
 }
