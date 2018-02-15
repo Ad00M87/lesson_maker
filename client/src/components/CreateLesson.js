3e0e7bee4aa11fea6271707fb2d3e1eabc072d40
Adam Kimball <adamchasekimball@gmail.com>
2018-02-14 22:52:46 -0700

    adding spacing to creator

--- client/src/components/CreateLesson.js
+++ client/src/components/CreateLesson.js
@@ -4,22 +4,28 @@ import { Header, Segment, Divider } from 'semantic-ui-react';
 class CreateLesson extends React.Component {
 
   //consider using reusable components so a user can select what they would like
-  //to add to a lesson/presentation. 
+  //to add to a lesson/presentation.
 
   render() {
     return(
       <Segment>
         <Header as='h1'>Lesson Creator</Header>
         <Divider />
+        <Divider />
         <Header as='h3'>Add a title</Header>
+        <Divider />
         <Divider />
         <Header as='h3'>Add a definition</Header>
         <Divider />
+        <Divider />
         <Header as='h3'>Add a video clip</Header>
+        <Divider />
         <Divider />
         <Header as='h3'>Add supporting text</Header>
         <Divider />
+        <Divider />
         <Header as='h3'>Add an image</Header>
+        <Divider />
         <Divider />
         <Header as='h3'>Add a concluding statement</Header>
       </Segment>
